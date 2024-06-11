5393645a2bef359aba24c0a5cc10d16872308fab
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2024-06-11 10:08:11 +0200

    lens optics matrices added

--- src/scripts/test_coro_layout.py
+++ src/scripts/test_coro_layout.py
@@ -10,6 +10,46 @@ fig, ax = plt.subplots(1,1,figsize=figsize, facecolor='lightskyblue')
 # for figure placement, I want to use the centre of the inserted figures as a coordinate, along with a width and height of the figure
 # where we ALSO make sure the axes are the same scale. For rectangular figures, this doesn't work, so we normalise to the height of the Figure so that the new coord system y-axis goes from -0.5 to 0.5 and the x axis is scaled so that it can go greater than unity.
 
+
+
+
+
+
+def s(d):
+	'FREE SPACE: d - distance'
+	return np.array([[1 , d],
+    	[0, 1]])
+
+def f(f):
+	'LENS: f - focal length'
+	return np.array([[1 , 0],
+    	[-1/f, 1]])
+
+
+# three x postions, three angles
+fp1 = np.array([[0.1, 0.1, 0.1, 0.0, 0.0, 0.0,-0.1,-0.1,-0.1],[-0.1,0.0,0.1,-0.1,0.0,0.1, -0.1,0.0,0.1]])
+
+to_lens1 = np.matmul(s(5),fp1)
+
+lens1 = np.matmul(f(5),to_lens1)
+to_pp1 = np.matmul(s(5),lens1)
+to_lens2 = np.matmul(s(5),to_pp1)
+lens2 = np.matmul(f(5),to_lens2)
+to_fp2 = np.matmul(s(5),lens2)
+
+print(fp1)
+print(to_lens1)
+print(lens1)
+print(to_pp1)
+
+print(to_fp2.shape)
+
+rays = np.stack((fp1,to_lens1,to_pp1,to_lens2,to_fp2))
+
+print(rays.shape)
+
+quit() 
+
 def placid(xc,yc,xs,ys,fig):
 	'placid - given centre (xc,yc) and side lengths (xs,ys) and Figure return 4-tuple suitable for Figure.add_axes'
 
@@ -56,6 +96,8 @@ y0 = 0.4
 
 rayheight = 0.2
 
+
+# 
 
 
 
