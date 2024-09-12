ccb85f5626be9cce64122d05c786dc7c0e3a0065
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2024-09-12 15:31:52 +0200

    fits moving

--- src/scripts/plot_aplc.py
+++ src/scripts/plot_aplc.py
@@ -1,87 +1,88 @@
 from hcipy import *
 import numpy as np
 from matplotlib import pyplot as plt
+import paths
 
-if __name__ == "__main__":
-	
-	grid = make_pupil_grid(512, 1.1)
-	aperture_func = make_circular_aperture(1)
-	aperture = evaluate_supersampled(aperture_func, grid, 4)
+grid = make_pupil_grid(512, 1.1)
+aperture_func = make_circular_aperture(1)
+aperture = evaluate_supersampled(aperture_func, grid, 4)
 
-	lyot_mask = evaluate_supersampled(make_circular_aperture(0.95), grid, 4)
-	lyot_stop = Apodizer(lyot_mask)
+lyot_mask = evaluate_supersampled(make_circular_aperture(0.95), grid, 4)
+lyot_stop = Apodizer(lyot_mask)
 
-	# focal plane parameters
-	iwa = 3.5
-	owa = 10.
-	target_contrast = 1e-8
+# focal plane parameters
+iwa = 3.5
+owa = 10.
+target_contrast = 1e-8
 
-	focal_grid = make_focal_grid(q=5, num_airy=owa+5)
-	prop = FraunhoferPropagator(grid, focal_grid)
+focal_grid = make_focal_grid(q=5, num_airy=owa+5)
+prop = FraunhoferPropagator(grid, focal_grid)
 
-	# Make the wavefront
-	wf = Wavefront(aperture)
-	wf.total_power = 1
-	norm = prop(wf).power.max()
-	
-	aplc_amp = read_field('./Data/aplc_amplitude_example.fits')
-	pre_apodizer = Apodizer(aplc_amp)
+# Make the wavefront
+wf = Wavefront(aperture)
+wf.total_power = 1
+norm = prop(wf).power.max()
 
-	# Make the focal plane mask
-	fpm_diameter = 6
-	cor_grid = make_focal_grid(q=25, num_airy=1.1 * fpm_diameter / 2)
-	focal_plane_mask = 1 - evaluate_supersampled(make_circular_aperture(fpm_diameter), cor_grid, 4)
+aplc_amp = read_field(str(paths.data/'aplc_amplitude_example.fits'))
+#aplc_amp = read_field(paths.scripts/'aplc_amplitude_example.fits')
+pre_apodizer = Apodizer(aplc_amp)
 
-	lyot_coronagraph = LyotCoronagraph(grid, focal_plane_mask)
-	
-	# The intermediate focal grid
-	focal_plane_mask_low_res = Apodizer(1 - evaluate_supersampled(make_circular_aperture(fpm_diameter), focal_grid, 4))
+# Make the focal plane mask
+fpm_diameter = 6
+cor_grid = make_focal_grid(q=25, num_airy=1.1 * fpm_diameter / 2)
+focal_plane_mask = 1 - evaluate_supersampled(make_circular_aperture(fpm_diameter), cor_grid, 4)
 
-	#
-	wf_apodized = pre_apodizer(wf)
-	
-	# First focal plane wavefronts
-	wf_pre_fpm = prop(wf_apodized)
-	wf_post_fpm = focal_plane_mask_low_res(wf_pre_fpm)
+lyot_coronagraph = LyotCoronagraph(grid, focal_plane_mask)
 
-	# Lyot Plane wavefronts
-	wf_lyot = lyot_coronagraph(wf_apodized)
-	wf_post_lyot = lyot_stop(wf_lyot)
+# The intermediate focal grid
+focal_plane_mask_low_res = Apodizer(1 - evaluate_supersampled(make_circular_aperture(fpm_diameter), focal_grid, 4))
 
-	# Science focal plane wavefronts
-	wf_foc = prop(wf_post_lyot)
+#
+wf_apodized = pre_apodizer(wf)
 
-	corim = wf_foc.power / norm
+# First focal plane wavefronts
+wf_pre_fpm = prop(wf_apodized)
+wf_post_fpm = focal_plane_mask_low_res(wf_pre_fpm)
 
-	plt.figure(figsize=(14,8))
+# Lyot Plane wavefronts
+wf_lyot = lyot_coronagraph(wf_apodized)
+wf_post_lyot = lyot_stop(wf_lyot)
 
-	plt.subplot(2,4,1)
-	plt.title('Apodizer')
-	imshow_field(aplc_amp, grid, cmap='gray', vmin=0, vmax=1)
-	plt.subplot(2,4,2)
-	plt.title('Focal plane mask')
-	imshow_field(focal_plane_mask_low_res.apodization, focal_grid, cmap='gray', vmin=0, vmax=1)
+# Science focal plane wavefronts
+wf_foc = prop(wf_post_lyot)
 
-	plt.subplot(2,4,5)
-	plt.title('PSF before focal plane mask')
-	imshow_psf(wf_pre_fpm.power / norm, vmax=1, vmin=target_contrast, colorbar=False)
-	plt.subplot(2,4,6)
-	plt.title('PSF after focal plane mask')
-	imshow_psf(wf_post_fpm.power / norm + 1e-15, vmax=1, vmin=target_contrast, colorbar=False)
+corim = wf_foc.power / norm
 
-	plt.subplot(2,4,3)
-	plt.title('Lyot Stop')
-	imshow_field(lyot_stop.apodization, grid, vmin=0, vmax=1, cmap='gray')
-	plt.subplot(2,4,4)
-	plt.title('Post-coronagraphic stellar PSF')
-	imshow_psf(corim, vmax=1, vmin=target_contrast, colorbar=False)
+plt.figure(figsize=(14,8))
 
-	plt.subplot(2,4,7)
-	plt.title('Star light before Lyot stop')
-	imshow_field(wf_lyot.power / wf_lyot.power.max(), vmin=0, vmax=1, cmap='inferno')
-	plt.subplot(2,4,8)
-	plt.title('Star light after Lyot stop')
-	imshow_field(wf_post_lyot.power / wf_lyot.power.max(), vmin=0, vmax=1, cmap='inferno')
-	
-	plt.tight_layout()
-	plt.show()
+plt.subplot(2,4,1)
+plt.title('Apodizer')
+imshow_field(aplc_amp, grid, cmap='gray', vmin=0, vmax=1)
+plt.subplot(2,4,2)
+plt.title('Focal plane mask')
+imshow_field(focal_plane_mask_low_res.apodization, focal_grid, cmap='gray', vmin=0, vmax=1)
+
+plt.subplot(2,4,5)
+plt.title('PSF before focal plane mask')
+imshow_psf(wf_pre_fpm.power / norm, vmax=1, vmin=target_contrast, colorbar=False)
+plt.subplot(2,4,6)
+plt.title('PSF after focal plane mask')
+imshow_psf(wf_post_fpm.power / norm + 1e-15, vmax=1, vmin=target_contrast, colorbar=False)
+
+plt.subplot(2,4,3)
+plt.title('Lyot Stop')
+imshow_field(lyot_stop.apodization, grid, vmin=0, vmax=1, cmap='gray')
+plt.subplot(2,4,4)
+plt.title('Post-coronagraphic stellar PSF')
+imshow_psf(corim, vmax=1, vmin=target_contrast, colorbar=False)
+
+plt.subplot(2,4,7)
+plt.title('Star light before Lyot stop')
+imshow_field(wf_lyot.power / wf_lyot.power.max(), vmin=0, vmax=1, cmap='inferno')
+plt.subplot(2,4,8)
+plt.title('Star light after Lyot stop')
+imshow_field(wf_post_lyot.power / wf_lyot.power.max(), vmin=0, vmax=1, cmap='inferno')
+
+plt.tight_layout()
+plt.savefig(paths.figures / 'aplc.pdf')
+#plt.show()
--- src/scripts/plot_app.py
+++ src/scripts/plot_app.py
@@ -1,62 +1,61 @@
 from hcipy import *
 import numpy as np
 from matplotlib import pyplot as plt
+import paths
 
-if __name__ == "__main__":
-	
-	grid = make_pupil_grid(256, 1.1)
-	aperture_func = make_circular_aperture(1)
-	aperture = evaluate_supersampled(aperture_func, grid, 4)
-	aperture_mask = aperture > 0
+grid = make_pupil_grid(256, 1.1)
+aperture_func = make_circular_aperture(1)
+aperture = evaluate_supersampled(aperture_func, grid, 4)
+aperture_mask = aperture > 0
 
-	##
-	iwa = 2.0
-	owa = 10.
-	target_contrast = 1e-5
+##
+iwa = 2.0
+owa = 10.
+target_contrast = 1e-5
 
-	focal_grid = make_focal_grid(q=5, num_airy=owa+5)
-	prop = FraunhoferPropagator(grid, focal_grid)
+focal_grid = make_focal_grid(q=5, num_airy=owa+5)
+prop = FraunhoferPropagator(grid, focal_grid)
 
-	# Make the wavefront
-	wf = Wavefront(aperture)
-	wf.total_power = 1
-	norm = prop(wf).power.max()
-	
-	app_phase = read_field('./Data/app_phase_example.fits')
-	app = PhaseApodizer(app_phase)
+# Make the wavefront
+wf = Wavefront(aperture)
+wf.total_power = 1
+norm = prop(wf).power.max()
 
-	wf_foc = prop(app(wf))
-	corim = wf_foc.power / norm
+app_phase = read_field(str(paths.data/'app_phase_example.fits'))
+app = PhaseApodizer(app_phase)
 
-	plt.figure(figsize=(14,8))
+wf_foc = prop(app(wf))
+corim = wf_foc.power / norm
 
-	plt.subplot(2,4,1)
-	plt.title('No apodizer')
-	imshow_field(grid.ones(), grid, cmap='gray', vmin=0, vmax=1)
-	plt.subplot(2,4,2)
-	plt.title('No focal plane mask')
-	imshow_field(focal_grid.ones(), focal_grid, cmap='gray', vmin=0, vmax=1)
+plt.figure(figsize=(14,8))
 
-	plt.subplot(2,4,5)
-	plt.title('PSF before focal plane mask')
-	imshow_psf(prop(wf).power / norm, vmax=1, vmin=target_contrast, colorbar=False)
-	plt.subplot(2,4,6)
-	plt.title('PSF after focal plane mask')
-	imshow_psf(prop(wf).power / norm, vmax=1, vmin=target_contrast, colorbar=False)
+plt.subplot(2,4,1)
+plt.title('No apodizer')
+imshow_field(grid.ones(), grid, cmap='gray', vmin=0, vmax=1)
+plt.subplot(2,4,2)
+plt.title('No focal plane mask')
+imshow_field(focal_grid.ones(), focal_grid, cmap='gray', vmin=0, vmax=1)
 
-	plt.subplot(2,4,3)
-	plt.title('APP phase pattern')
-	imshow_field(app.phase, grid, vmin=-np.pi, vmax=np.pi, cmap='twilight')
-	plt.subplot(2,4,4)
-	plt.title('Post-coronagraphic stellar PSF')
-	imshow_psf(corim, vmax=1, vmin=target_contrast, colorbar=False)
+plt.subplot(2,4,5)
+plt.title('PSF before focal plane mask')
+imshow_psf(prop(wf).power / norm, vmax=1, vmin=target_contrast, colorbar=False)
+plt.subplot(2,4,6)
+plt.title('PSF after focal plane mask')
+imshow_psf(prop(wf).power / norm, vmax=1, vmin=target_contrast, colorbar=False)
 
-	plt.subplot(2,4,7)
-	plt.title('Star light before Lyot stop')
-	imshow_field(wf.power / wf.power.max(), vmin=0, vmax=1, cmap='inferno')
-	plt.subplot(2,4,8)
-	plt.title('Star light after Lyot stop')
-	imshow_field(wf.power / wf.power.max(), vmin=0, vmax=1, cmap='inferno')
-	
-	plt.tight_layout()
-	plt.show()
+plt.subplot(2,4,3)
+plt.title('APP phase pattern')
+imshow_field(app.phase, grid, vmin=-np.pi, vmax=np.pi, cmap='twilight')
+plt.subplot(2,4,4)
+plt.title('Post-coronagraphic stellar PSF')
+imshow_psf(corim, vmax=1, vmin=target_contrast, colorbar=False)
+
+plt.subplot(2,4,7)
+plt.title('Star light before Lyot stop')
+imshow_field(wf.power / wf.power.max(), vmin=0, vmax=1, cmap='inferno')
+plt.subplot(2,4,8)
+plt.title('Star light after Lyot stop')
+imshow_field(wf.power / wf.power.max(), vmin=0, vmax=1, cmap='inferno')
+
+plt.tight_layout()
+plt.savefig(paths.figures/'app.pdf')
