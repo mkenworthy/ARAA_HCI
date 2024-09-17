3f5b00fefdeb88b0adb94e9a3f4ddcec2128693c
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2024-09-17 06:46:53 +0200

    phasing errors

--- src/scripts/segment_phasing_errors.py
+++ src/scripts/segment_phasing_errors.py
@@ -9,12 +9,15 @@ grid = make_pupil_grid(256, 42.0)
 aperture_function, segment_functions = make_elt_aperture(return_segments=True)
 aperture = evaluate_supersampled(aperture_function, grid, 4)
 
-# Try to read the segments file otherwise create it.
+#Try to read the segments file otherwise create it.
 try:
 	segments = read_field(str(paths.data/'elt_segments.fits'))
 except:
 	segments = Field([evaluate_supersampled(seg, grid, 4) for seg in segment_functions], grid)
 	write_field(segments, str(paths.data/'elt_segments.fits'))
+
+
+#segments = Field([evaluate_supersampled(seg, grid, 4) for seg in segment_functions], grid)
 	
 num_segments = segments.shape[0]
 
@@ -34,21 +37,26 @@ ff = FourierFilter(grid, make_circular_aperture(2 * np.pi * 60 / Dtel))
 abb.surface_sag = abb.surface_sag - np.real(ff.forward(abb.surface_sag + 0j)) 
 
 rms = np.logspace(-1.5, -0.5, 3)
-plt.figure(figsize=(14,8))
 
-for i, rms_i in enumerate(rms):
-	phasing_errors.phase = segments.T.dot( rms_i * np.random.randn(num_segments) )
+fig, axes =plt.subplots(1,3,figsize=(12,4))
 
-	corim = prop( coronagraph( phasing_errors(abb(wf) ) ) ).power / norm
-	psf = prop( phasing_errors(abb(wf) ) ).power / norm
+ax = np.ndarray.flatten(axes)
+#for i, rms_i in enumerate(rms):
 
-	plt.subplot(3, 3, 1 + 3 * (2 - i))
-	imshow_field(phasing_errors.phase, vmin=-np.pi, vmax=np.pi,cmap='bwr')
-	plt.colorbar()
-	plt.subplot(3, 3, 2 + 3 * (2 - i))
-	imshow_psf(psf, vmax=1, vmin=1e-5)
-	plt.subplot(3, 3, 3 + 3 * (2 - i))
-	imshow_psf(corim, vmax=1e-2, vmin=1e-7)
+rms_i = 0.32
+
+phasing_errors.phase = segments.T.dot( rms_i * np.random.randn(num_segments) )
+
+corim = prop( coronagraph( phasing_errors(abb(wf) ) ) ).power / norm
+psf = prop( phasing_errors(abb(wf) ) ).power / norm
+
+imshow_field(phasing_errors.phase, vmin=-np.pi/2, vmax=np.pi/2,cmap='bwr',ax=ax[0])
+ax[0].set_title(f'Phase error r.m.s.={rms_i:4.2f}$\pi$ radians')
+plt.colorbar()
+imshow_psf(psf, vmax=1, vmin=1e-5,ax=ax[1])
+ax[1].set_title('ELT PSF')
+ax[2].set_title('Coronagraphic PSF')
+imshow_psf(corim, vmax=1e-2, vmin=1e-7,ax=ax[2])
 
 plt.tight_layout()
 plt.draw()
